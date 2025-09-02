735c4ae4759b0ba302fd2e1a2fee8654e0bf7be7
Grantpriest <grantpriest@gmail.com>
2025-09-02 17:52:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/os_ipad4/', // ← IMPORTANT: must match your GitHub repo name
+  base: '/os_ipad4/',
   plugins: [react()],
-});
+})
