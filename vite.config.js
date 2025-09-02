48e04c7c3c0a2c05c8903ae9c7d11ca3e568497b
Grantpriest <grantpriest@gmail.com>
2025-09-02 10:22:45 +0900

    fix4

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/OS_Jukebox/',
+  base: '/os_ipad4/',
   plugins: [react()],
 });
